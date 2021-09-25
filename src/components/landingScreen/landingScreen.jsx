d1b2d18c869605a867e9893d8dc5f8fd853148f1
Samrah Sajid <samrah.sajid@rekd-app.com>
2021-09-25 19:16:04 +0500

    Create scaffolding for landing screen component

---
+++ src/components/landingScreen/landingScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Styles from './landingScreen.module.scss';
+
+class LandingPage extends React.Component {
+	render() {
+		return (
+			<div className={Styles.mainScreen}>
+				jkansdkajnsjkdn
+			</div>
+		);
+	}
+}
+
+export default LandingPage;
