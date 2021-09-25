81ad85284ed036beb8861cc816bf5a7b846b4c75
Samrah Sajid <samrah.sajid@rekd-app.com>
2021-09-25 20:30:44 +0500

    Add landing screen content

--- src/components/landingScreen/landingScreen.jsx
+++ src/components/landingScreen/landingScreen.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import Parallax from 'parallax-js';
+import gsap from 'gsap';
 import Styles from './landingScreen.module.scss';
+import LemonFull from '../svgComponents/lemonFull';
+import LemonHalf from '../svgComponents/lemonHalf';
+import LemonQuarter from '../svgComponents/lemonQuarter';
+import Petal from '../svgComponents/petal';
+import Flower from '../svgComponents/flower';
+import Logo from '../svgComponents/logo';
 
 class LandingPage extends React.Component {
+	componentDidMount() {
+		const scene = document.getElementById('scene');
+		const parallax = new Parallax(scene);
+
+		parallax.friction(0.01, 0.01);
+	}
+
+	pageTransition() {
+		gsap.to('#logo', { opacity: 0 });
+		gsap.to('#landingBlob', {
+			scale: 10,
+			onComplete: () => {
+				this.props.history.push('/lemon-cake');
+			}
+		}, '<0.5');
+	}
+
 	render() {
 		return (
 			<div className={Styles.mainScreen}>
-				jkansdkajnsjkdn
+				<div id="landingBlob" onClick={() => { this.pageTransition(); }} className={Styles.blob}></div>
+				<Logo ID="logo" styles={Styles.logo} />
+				<div id="scene" className={Styles.parallaxContainer}>
+					<div data-depth="-0.5" className={Styles.scene}>
+						<div className={Styles.lemonFull1}><LemonFull /></div>
+						<div className={Styles.lemonFull2}><LemonFull /></div>
+						<div className={Styles.lemonHalf1}><LemonHalf /></div>
+						<div className={Styles.lemonHalf2}><LemonHalf /></div>
+						<div className={Styles.lemonQuarter1}><LemonQuarter /></div>
+						<div className={Styles.lemonQuarter2}><LemonQuarter /></div>
+						<div className={Styles.petal1}><Petal /></div>
+						<div className={Styles.petal2}><Petal /></div>
+						<div className={Styles.flower1}><Flower /></div>
+						<div className={Styles.flower2}><Flower /></div>
+						<div className={Styles.flower3}><Flower /></div>
+						<div className={Styles.flower4}><Flower /></div>
+					</div>
+				</div>
 			</div>
 		);
 	}
