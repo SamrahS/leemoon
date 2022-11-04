7c6bba605e72f2cf8514bbb93c2d203a5995fe2b
Samrah Sajid <samrahsajid16@gmail.com>
2022-11-04 15:11:13 +0500

    Create scaffolding fro products screen

---
+++ src/components/productsScreen/productsScreen.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import gsap from 'gsap';
+import Styles from './productsScreen.module.scss';
+
+function ProductsScreen() {
+	// const navigate = useNavigate();
+
+	React.useEffect(() => {
+	}, []);
+
+	return (
+		<div className={Styles.mainScreen}>
+		</div>
+	);
+}
+
+export { ProductsScreen };
