41b1b1d0a7ef1c2303fbbaf156baf10428944477
Abdul Basit <69422801+abdulbasit64@users.noreply.github.com>
2022-11-06 16:43:41 +0500

    add product - lemon cake

---
+++ src/components/productsScreen/lemonCake/lemonCake.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Parallax from 'parallax-js';
+import cake from '../../../assets/cake.png';
+import dots from '../../../assets/polka-dots.png';
+import Styles from './lemonCake.module.scss';
+
+function LemonCake() {
+	React.useEffect(() => {
+		const product = document.getElementById('imagesContainer');
+		const parallax = new Parallax(product);
+		parallax.friction(0.015, 0.015);
+	}, []);
+
+	return (
+		<div className={Styles.mainScreen}>
+			<div id='imagesContainer' className={Styles.imagesContainer}>
+				<img data-depth='-0.4' className={Styles.dots} src={dots} alt='bg' />
+			</div>
+			<img
+				className={Styles.cake}
+				src={cake}
+				alt='lemon_cake'
+			/>
+			<div className={Styles.descContainer}>
+				<h1>Ye hai Lemon cake</h1>
+				<h2>VIP Lemon cake</h2>
+			</div>
+		</div>
+	);
+}
+
+export { LemonCake };
