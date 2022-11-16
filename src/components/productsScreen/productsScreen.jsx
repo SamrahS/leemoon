54d3a2d6bd691689c717fffac48096a0d68af5cf
Samrah Sajid <samrahsajid16@gmail.com>
2022-11-16 21:23:58 +0500

    Update products screen

--- src/components/productsScreen/productsScreen.jsx
+++ src/components/productsScreen/productsScreen.jsx
@@ -1,79 +1,34 @@
 import React from 'react';
-// import { LemonCake } from './lemonCake/lemonCake';
+import Slider from 'react-slick';
+import Logo from '../svgComponents/logo';
 // import { useNavigate } from 'react-router-dom';
-import gsap from 'gsap';
 import Styles from './productsScreen.module.scss';
 
 function ProductsScreen() {
 	// const navigate = useNavigate();
 
-	const [section, setSection] = React.useState();
-
-	React.useEffect(() => {
-		const sectionTimeline = gsap.timeline();
-		const animationProps = {
-			x: 10,
-			opacity: 1,
-			duration: 0.3
-		};
-
-		sectionTimeline.to('#productSection_3', animationProps)
-			.to('#productSection_2', animationProps, '<0.1')
-			.to('#productSection_1', animationProps, '<0.1')
-			.to('#productSection_0', {
-				...animationProps,
-				onComplete: () => {
-					setTimeout(() => {
-						setSection(0);
-					}, 500);
-				}
-			}, '<0.1');
-	}, []);
-
-	const sectionProps = (index) => ({
-		id: `productSection_${index}`,
-		className: `${Styles.section} ${section === index ? Styles.selected : null}`,
-		onClick: () => setSection(index)
-	});
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1
+	};
 
 	return (
 		<div className={Styles.mainScreen}>
-			<div {...sectionProps(0)}>
-				<p>LEMON CAKE</p>
-				<div className={Styles.sectionContent}>
-					<h4>LEMON<br/>CAKE</h4>
-					<div className={Styles.imageContainer}>
-						<div className={Styles.imageShadow} />
-					</div>
-				</div>
+			<Logo id="ProductsScreenLogo" styles={Styles.logo} />
+			<div className={Styles.bgContainer}>
+				<div className={Styles.bgContent}></div>
 			</div>
-			<div {...sectionProps(1)}>
-				<p>LEMON CUPCAKE</p>
-				<div className={Styles.sectionContent}>
-					<h4>LEMON<br/>CUPCAKE</h4>
-					<div className={Styles.imageContainer}>
-						<div className={Styles.imageShadow} />
-					</div>
+			<Slider {...settings}>
+				<div className={Styles.section}>
+					<p>kansdkajnd</p>
 				</div>
-			</div>
-			<div {...sectionProps(2)}>
-				<p>LEMON TART</p>
-				<div className={Styles.sectionContent}>
-					<h4>LEMON<br/>TART</h4>
-					<div className={Styles.imageContainer}>
-						<div className={Styles.imageShadow} />
-					</div>
+				<div className={Styles.section}>
+					<p>kansdkajnd</p>
 				</div>
-			</div>
-			<div {...sectionProps(3)}>
-				<p>LEMON TARTLET</p>
-				<div className={Styles.sectionContent}>
-					<h4>LEMON<br/>TARTLET</h4>
-					<div className={Styles.imageContainer}>
-						<div className={Styles.imageShadow} />
-					</div>
-				</div>
-			</div>
+			</Slider>
 		</div>
 	);
 }
